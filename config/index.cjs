53fbb3eed35acf25e1ba561370d8ee8cef65bf1b
15554041980 <95633655+15554041980@users.noreply.github.com>
2023-10-09 09:16:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,17 +14,21 @@ const USER_CONFIG = {
   APP_SECRET: 'ba997a5dd401d5826d7e00b7c043f75e',
 
   PROVINCE: '北京',
-  CITY: '丰台',
+  CITY: '丰台区',
 
   USERS: [
     {
       // 想要发送的人的名字
+      
       name: '琴宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofkwa6uUWqsvwXY9yAdDJejBjygA',
+      
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'qrILeD-ayh0hie43AlBzvp5tzwtb8QdZJZxARUU18j0',
+     
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+     
       horoscopeDate: '4-14',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -55,7 +59,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '主人',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofkwa6uUWqsvwXY9yAdDJejBjygA',
     }
