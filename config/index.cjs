197456e19eed0e08f7d4b9e9ab6d807929f59570
15554041980 <95633655+15554041980@users.noreply.github.com>
2023-10-11 09:40:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -71,6 +71,30 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-01' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '娜娜',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ofkwa6vlNKzUU2lXRekDnkuvX7Rg',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'Db2eyCe3ZClNy_EIbN0NBNFqhiLX86Cm96YQmgno4tg',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '9-09',
+      festivals: [
+        
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '娜娜', year: '2002', date: '09-09',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '09-01',
+        },
+      ],
+      // 从我们遇见到现在已经有xxxx天了的配置
+      customizedDateList: [
+        // 第一次见面
+        { keyword: 'marry_day', date: '2022-09-01' },
+      ],
   ],
 
 
