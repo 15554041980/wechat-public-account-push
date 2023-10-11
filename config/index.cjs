34ae75b69c22c8eee0bf7a00cb364a4ce404e010
15554041980 <95633655+15554041980@users.noreply.github.com>
2023-10-11 08:33:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -35,24 +35,6 @@ const USER_CONFIG = {
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '09-01',
         },
-        {
-      // 想要发送的人的名字
-      name: '洋洋',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ofkwa6uUWqsvwXY9yAdDJejBjygA',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qrILeD-ayh0hie43AlBzvp5tzwtb8QdZJZxARUU18j0',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '1-09',
-      festivals: [
-        
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '洋洋', year: '2003', date: '01-09',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '09-01',
-        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
