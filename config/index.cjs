40783838195ab012599e0723def79b9bb00cfdcc
15554041980 <95633655+15554041980@users.noreply.github.com>
2023-10-11 09:46:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -77,7 +77,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ofkwa6vlNKzUU2lXRekDnkuvX7Rg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Db2eyCe3ZClNy_EIbN0NBNFqhiLX86Cm96YQmgno4tg',
+      useTemplateId: 'fX3Wgt6W2WIJ8J6tX_vvfOADqzn6uaTjH09xChdmFZY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '9-09',
       festivals: [
@@ -95,6 +95,7 @@ const USER_CONFIG = {
         // 第一次见面
         { keyword: 'marry_day', date: '2022-09-01' },
       ],
+    },
   ],
 
 
